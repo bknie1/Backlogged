1a7b196fb400048d26b31831f9df79c0acb8cd6b
bknie1 <brandonknieriem26@gmail.com>
2020-03-21 01:45:01 +0000

    Added load games from DB, sort alphabetically, placeholder cover art if no art provided.

--- app.js
+++ app.js
@@ -5,27 +5,30 @@ const port = 3000;
 
 app.use(express.static("css"));
 app.set("view engine", "ejs");
-// Body Parser --------------------------------------------------
+// Body Parser -------------------------------------------------
 const bp = require("body-parser");
 app.use(bp.urlencoded({extend: true}));
-// Mongoose -----------------------------------------------------
+
+// Mongoose ----------------------------------------------------
 const mongoose = require("mongoose");
 
 mongoose.connect('mongodb://localhost/Gamezy', {useNewUrlParser: true, useUnifiedTopology: true});
 
-var db = mongoose.connection;
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
   console.log("Connected to database.");
 });
-// Schemas ------------------------------------------------------
-var gameSchema = new mongoose.Schema({
+
+// Schemas -----------------------------------------------------
+const gameSchema = new mongoose.Schema({
 	name: String,
 	system: String,
-	image: String
+	image: String,
+	tags: String
 });
 
-var Game = mongoose.model("Game", gameSchema);
+const Game = mongoose.model("Game", gameSchema);
 // ROUTES -------------------------------------------------------
 
 app.get("/", (req, res) => {
@@ -33,11 +36,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/games", (req, res) => {
-	// let games = getAllGames();
-	
-	let games = [{"name": "Crash Bandicoot", "system": "PlayStation", "image": "#"}];
-	
-	res.render("games", {ViewModel: games});
+	getGamesAndRenderView(req, res);
 });
 
 app.get("/games/new", (req, res) => {
@@ -58,43 +57,55 @@ app.get("*", (req, res) => {
 	res.redirect("/");
 });
 
-// METHODS ------------------------------------------------------
-function getAllGames() {
-	let games = {};
-	
-	return games;
-}
-
-function addGame(title, system, imageUrl) {
-	// If unique title and system, add.
-	Game.find({name: title, system: system}, (err, games) => {
-		if (err) return console.error(err);
-  		console.log(games);
-		
-		if(games === undefined || games.length == 0) {
-			console.log("Unique game.");
-			let newGame = new Game({
-				name: title, 
-				system: system, 
-				image: imageUrl
-			});
-
-			newGame.save((err, g) => {
-				if(err) {
-					console.log(err);
-				} else {
-					console.log(`Added ${g}`);
-				}
-			});
+// METHODS -----------------------------------------------------
+function getGamesAndRenderView(req, res) {
+	/// Get all games or a specific game by title.
+	Game.find({}, null, {sort: {name: 1}}).lean().exec((err, docs) => {
+		if(err) {
+		  console.log("Error: Could not find games.")
 		} else {
-			console.log("Error: Duplicate game.");
+			console.log(docs);
+			res.render("games", {ViewModel: docs});
 		}
 	});
 }
 
-function removeByTitle(title, system) {
-	Property.findOneAndRemove({title: title, system: system}, (err,data) => { if(!err){ console.log(`Deleted ${title}`); }});
+function addGame(title, system, imageUrl) {
+	/// If unique title and system, add. Otherwise, update.
+	const filter = {name: title, system: system};
+	
+	if(Game.exists(filter)) {
+		// INSERT Game
+		if(!imageUrl.length) { // Placeholder Image
+			imageUrl = "https://via.placeholder.com/150/000000/FFFFFF/?text=No%20Image"
+		}
+
+		let newGame = new Game({
+			name: title, 
+			system: system, 
+			image: imageUrl
+		});
+
+		newGame.save((err, g) => {
+			if(err) {
+				console.log(err);
+			} else {
+				console.log(`Added ${g}`);
+			}
+		});
+	} else {
+		console.log("Duplicate entry. Updating.");
+		updateGame(title, system, imageUrl);
+	}
 }
 
-// LISTEN -------------------------------------------------------
+function updateGame(title, system, imageUrl) {
+	Game.updateOne(filter, { name: title, system: system, image: imageUrl });
+}
+
+function removeByTitle(title, system) {
+	Game.findOneAndRemove({title: title, system: system}, (err,data) => { if(!err){ console.log(`Deleted ${title}`); }});
+}
+
+// LISTEN ------------------------------------------------------
 app.listen(port, () => { console.log(`Listening on port ${port}`); });
