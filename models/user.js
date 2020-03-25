e051c01a2ebe1650598c17d49e944bec6c3578dc
bknie1 <brandonknieriem26@gmail.com>
2020-03-25 21:32:33 +0000

    Added authentication dependent view states.

--- models/user.js
+++ models/user.js
@@ -1,8 +1,12 @@
 const mongoose = require("mongoose");
+const plm = require("passport-local-mongoose");
 
 const userSchema = new mongoose.Schema({
 	username: String,
+	password: String,
 	email: String
 });
+
+userSchema.plugin(plm);
 
 module.exports = new mongoose.model("User", userSchema);
